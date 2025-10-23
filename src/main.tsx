626c10d1c75593c3d1a64de43bd725599377f4d9
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2025-10-22 21:21:13 -0300

    T2SQL-232 Remove Tanstack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
-import * as TanStackQueryProvider from '@/integrations/tanstack-query/root-provider.tsx'
 import { routeTree } from '@/routeTree.gen'
 import '@/styles.css'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
-const TanStackQueryProviderContext = TanStackQueryProvider.getContext()
 const router = createRouter({
   routeTree,
-  context: {
-    ...TanStackQueryProviderContext,
-  },
   defaultPreload: 'intent',
   scrollRestoration: true,
   defaultStructuralSharing: true,
@@ -28,9 +23,7 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <TanStackQueryProvider.Provider {...TanStackQueryProviderContext}>
-        <RouterProvider router={router} />
-      </TanStackQueryProvider.Provider>
+      <RouterProvider router={router} />
     </StrictMode>,
   )
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,31 +1,11 @@
 import Header from '@/components/header'
-import TanStackQueryDevtools from '@/integrations/tanstack-query/devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-import type { QueryClient } from '@tanstack/react-query'
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
 
-interface MyRouterContext {
-  queryClient: QueryClient
-}
-
-export const Route = createRootRouteWithContext<MyRouterContext>()({
+export const Route = createRootRoute({
   component: () => (
     <div className="h-screen overflow-hidden">
       <Header />
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
     </div>
   ),
 })
